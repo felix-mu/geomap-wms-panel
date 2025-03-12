19e523de097d2a208f958dbc4eca499e56113bda
Felix Müller <felixrelleum@googlemail.com>
2025-03-12 17:11:53 +0100

    fix(utils.ts): use minimum threshold value in legend instead of infinity
    Source:
    https://github.com/grafana/grafana/blob/21b9d45ca6be144b284305bf3b27481555cf4ac1/public/app/core/components/TimelineChart/utils.ts#L488

--- src/layers/data/utils.ts
+++ src/layers/data/utils.ts
@@ -8,19 +8,50 @@ export function getThresholdItems(fieldConfig: FieldConfig, theme: GrafanaTheme2
     return items;
   }
 
+  // const steps = thresholds.steps;
+  // const disp = getValueFormat(thresholds.mode === ThresholdsMode.Percentage ? 'percent' : fieldConfig.unit ?? '');
+
+  // const fmt = (v: number) => formattedValueToString(disp(v));
+
+  // for (let i = 1; i <= steps.length; i++) {
+  //   const step = steps[i - 1];
+  //   items.push({
+  //     label: i === 1 ? `< ${fmt(step.value)}` : `${fmt(step.value)}+`,
+  //     color: theme.visualization.getColorByName(step.color),
+  //     yAxis: 1,
+  //   });
+  // }
+
+  // Taken from https://github.com/grafana/grafana/blob/main/public/app/core/components/TimelineChart/utils.ts#L488
+  // Do not use the infinity sign
   const steps = thresholds.steps;
-  const disp = getValueFormat(thresholds.mode === ThresholdsMode.Percentage ? 'percent' : fieldConfig.unit ?? '');
+  const getDisplay = getValueFormat(
+    thresholds.mode === ThresholdsMode.Percentage ? 'percent' : (fieldConfig.unit ?? '')
+  );
 
-  const fmt = (v: number) => formattedValueToString(disp(v));
+  // `undefined` value for decimals will use `auto`
+  const format = (value: number) => formattedValueToString(getDisplay(value, fieldConfig.decimals ?? undefined));
 
-  for (let i = 1; i <= steps.length; i++) {
-    const step = steps[i - 1];
+  for (let i = 0; i < steps.length; i++) {
+    let step = steps[i];
+    let value = step.value;
+    let pre = '';
+    let suf = '';
+
+    if (value === -Infinity && i < steps.length - 1) {
+      value = steps[i + 1].value;
+      pre = '< ';
+    } else {
+      suf = '+';
+    }
+
     items.push({
-      label: i === 1 ? `< ${fmt(step.value)}` : `${fmt(step.value)}+`,
+      label: `${pre}${format(value)}${suf}`,
       color: theme.visualization.getColorByName(step.color),
       yAxis: 1,
     });
   }
+
 
   return items;
 }
