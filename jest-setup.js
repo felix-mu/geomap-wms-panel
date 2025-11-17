71f60f6a7d89df379c1ed23384b95301aca95921
Felix Müller <felixrelleum@googlemail.com>
2025-11-17 23:50:22 +0100

    chore(jest): fix transformIgnorePatterns

--- jest-setup.js
+++ jest-setup.js
@@ -1,6 +1,10 @@
 // Jest setup provided by Grafana scaffolding
 import './.config/jest-setup';
-
 import path from 'path';
+import { TextEncoder, TextDecoder } from 'util';
 
 globalThis.PLUGIN_VERSION = require(path.resolve(process.cwd(), 'package.json')).version;
+
+
+globalThis.TextEncoder = TextEncoder;
+globalThis.TextDecoder = TextDecoder;
