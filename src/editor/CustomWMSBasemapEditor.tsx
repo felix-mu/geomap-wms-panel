a00e9dcf3932183d4dfc239427ed0561e342fa90
Felix Müller <felixrelleum@googlemail.com>
2026-01-04 01:42:50 +0100

    fix(customwmsbasemapeditor.tsx): fix violation of react-hooks/refs rule

--- src/editor/CustomWMSBasemapEditor.tsx
+++ src/editor/CustomWMSBasemapEditor.tsx
@@ -32,58 +32,52 @@ type Props = {
 
   // const options: Array<SelectableValue<string>> = [];
 
-export const CustomWMSBasemapEditor = ({ onChange, wms, cache }: Props) => {  
-  // const [url, setURL] = useState<string>(wms === undefined || 
-  //   wms.url === undefined ? "" : wms.url);
-  const url = useRef<string>(wms === undefined || 
+export const CustomWMSBasemapEditor = ({ onChange, wms, cache }: Props) => {
+  const [url, setURL] = useState<string>(wms === undefined || 
     wms.url === undefined ? "" : wms.url);
   const [attribution, setAttribution] = useState<string>(wms === undefined || 
     wms.attribution === undefined ? "" : wms.attribution);
-  // const [opacity, setOpacity] = useState<number>(wms === undefined || 
-  //   wms.opacity === undefined ? 1.0 : wms.opacity);
-  // const [showLegend, setShowLegend] = useState<boolean>(wms === undefined || 
-  //   wms.opacity === undefined ? true : wms.showLegend);
   const opacityRef = useRef<number>(wms === undefined || 
     wms.opacity === undefined ? 1.0 : wms.opacity);
   const [options, setOptions] = useState<Array<SelectableValue<string>>>([]);  // SelectableValue
   const [selection, setSelection] = useState<Array<SelectableValue<string>>>([]);
 
-  function handleLayers(layers: Array<SelectableValue<string>>) {
-    setOptions(layers);
-
-    // If layers are provided because of a repeating entry in edit mode while editing or after refresh
-    // set the selection to show the user the current layer selection
-    if (wms && wms.layers) {
-      let selection_tmp: Array<SelectableValue<string>> = [];
-      
-      // Generate selection from the available options by comparing the value keys with the layer names of the config
-      wms.layers.forEach(
-        (el) => {
-          selection_tmp = selection_tmp.concat(
-            // User layers since options are update in next render and therefore might be empty
-            layers.filter((currentValue) => {
-              return currentValue.value === el.name;
-            })
-          );
-        }
-      );
-      setSelection(selection_tmp);
-    } 
-  }
 
   // Update the select options when the url changes
   useEffect(() => {
-    if (cache.current[url.current]) {
-      handleLayers(cache.current[url.current]);
+    function handleLayers(layers: Array<SelectableValue<string>>) {
+      setOptions(layers);
+
+      // If layers are provided because of a repeating entry in edit mode while editing or after refresh
+      // set the selection to show the user the current layer selection
+      if (wms && wms.layers) {
+        let selection_tmp: Array<SelectableValue<string>> = [];
+        
+        // Generate selection from the available options by comparing the value keys with the layer names of the config
+        wms.layers.forEach(
+          (el) => {
+            selection_tmp = selection_tmp.concat(
+              // User layers since options are update in next render and therefore might be empty
+              layers.filter((currentValue) => {
+                return currentValue.value === el.name;
+              })
+            );
+          }
+        );
+        setSelection(selection_tmp);
+      } 
+    }
+    
+    if (cache.current[url]) {
+      handleLayers(cache.current[url]);
     } else {
-      getWMSCapabilitiesFromService(url.current).then(async (node) => {
+      getWMSCapabilitiesFromService(url).then(async (node) => {
         let layers = getWMSLayers(node);
-        cache.current[url.current] = layers;
+        cache.current[url] = layers;
         handleLayers(layers);
       }).catch(err => {});
     }
-    // eslint-disable-next-line
-  }, [url, wms]);
+  }, [url, wms, cache]);
 
   // <label className={styles.svg}>My select</label>
   // https://github.com/grafana/grafana/blob/ef5d71711a523efc65da089d45083e28201b58ab/packages/grafana-ui/src/components/Forms/Label.tsx
@@ -92,10 +86,10 @@ export const CustomWMSBasemapEditor = ({ onChange, wms, cache }: Props) => {
       <Label description={'URL to WMS endpoint (required)'}>
         URL
       </Label>
-      <Input value={url.current} aria-label="URL input"
+      <Input value={url} aria-label="URL input"
           onChange={e => {
-            // setURL(e.currentTarget.value);
-            url.current = e.currentTarget.value;
+            setURL(e.currentTarget.value);
+            // url.current = e.currentTarget.value;
             if (wms) {
               wms.layers.splice(-1);
             }
@@ -103,33 +97,33 @@ export const CustomWMSBasemapEditor = ({ onChange, wms, cache }: Props) => {
             setSelection([]);
             }} 
           onBlur={(e) => {
-              if (url.current === wms.url) {
+              if (url === wms.url) {
                 return;
               }
-              onChange({url: url.current!, layers: selection.map((e) => ({title: e.label!, name: e.value!})), attribution: attribution, opacity: wms.opacity, showLegend: wms.showLegend});
+              onChange({url: url, layers: selection.map((e) => ({title: e.label!, name: e.value!})), attribution: attribution, opacity: wms.opacity, showLegend: wms.showLegend});
             }}></Input>
       <Label description={'Select the layers to be displayed in base map'}>
         Layers
       </Label>
       <MultiCombobox aria-label="wms layer multiselect" options={options as Array<ComboboxOption<string>>} value={selection as Array<ComboboxOption<string>>} onChange={(selectableValue) => {
         setSelection(selectableValue);
-        onChange({url: url.current!, layers: selectableValue.map((e) => ({title: e.label!, name: e.value!})), attribution: attribution, opacity: wms.opacity, showLegend: wms.showLegend}); // onChange sets the config.wms property; Only change it when layers are selected
+        onChange({url: url, layers: selectableValue.map((e) => ({title: e.label!, name: e.value!})), attribution: attribution, opacity: wms.opacity, showLegend: wms.showLegend}); // onChange sets the config.wms property; Only change it when layers are selected
         }}></MultiCombobox>
       <Field label="Opacity" aria-label="wms opacity slider">
         <Slider value={wms.opacity} step={0.1} min={0} max={1} onAfterChange={(val) => {
-          onChange({url: url.current!, layers: selection.map((e) => ({title: e.label!, name: e.value!})), attribution: attribution, opacity: val, showLegend: wms.showLegend})
+          onChange({url: url, layers: selection.map((e) => ({title: e.label!, name: e.value!})), attribution: attribution, opacity: val, showLegend: wms.showLegend})
         }} onChange={(val) => {opacityRef.current = val}}></Slider>
       </Field>
       <Field label="Show legend" description="Toggle to show layers in legend" aria-label="wms layer legend toggle switch">
           <Switch value={(wms.showLegend || false)} onChange={(e) => {
-            onChange({url: url.current!, layers: selection.map((e) => ({title: e.label!, name: e.value!})), attribution: attribution, opacity: wms.opacity, showLegend: !wms.showLegend})
+            onChange({url: url, layers: selection.map((e) => ({title: e.label!, name: e.value!})), attribution: attribution, opacity: wms.opacity, showLegend: !wms.showLegend})
           }
           }/>
         </Field>
       <Field label="Attribution (optional)" /*description="This information is very important, so you really need to fill it in"*/>
         <Input value={attribution} aria-label="attribution input" onChange={e => {
           setAttribution(e.currentTarget.value);
-        }} onBlur={() => onChange({url: url.current!, layers: selection.map((e) => ({title: e.label!, name: e.value!})), attribution: attribution, opacity: opacityRef.current, showLegend: wms.showLegend})}></Input>
+        }} onBlur={() => onChange({url: url, layers: selection.map((e) => ({title: e.label!, name: e.value!})), attribution: attribution, opacity: opacityRef.current, showLegend: wms.showLegend})}></Input>
       </Field>
     </div>
         )
