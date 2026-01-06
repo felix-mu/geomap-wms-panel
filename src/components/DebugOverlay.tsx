2d2aef617e761d22e0f281cb74c311af15c9c49e
Felix Müller <felixrelleum@googlemail.com>
2026-01-06 23:14:07 +0100

    fix(debugoverlay.tsx): migrate component to function to allow hooks

--- src/components/DebugOverlay.tsx
+++ src/components/DebugOverlay.tsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, useEffect, useState } from 'react';
 import { Map } from 'ol';
 import { transform } from 'ol/proj';
 import { GrafanaTheme2 } from '@grafana/data';
@@ -12,55 +12,96 @@ interface Props {
   map: Map;
 }
 
-interface State {
+interface DebugOverlayState {
   zoom?: number;
   center: Coordinate;
 }
 
-export class DebugOverlay extends PureComponent<Props, State> {
-  style = useStyles2(getStyles);
+export function DebugOverlay({ map }: Props) {
+  const style = useStyles2(getStyles);
+  const [debugOverlayState, setDebugOverlayState] = useState<DebugOverlayState>({ zoom: 0, center: [0, 0] });
 
-  constructor(props: Props) {
-    super(props);
-    this.state = { zoom: 0, center: [0, 0] };
-  }
+  useEffect(() => {
+    const updateViewState = () => {
+      const view = map.getView();
+      setDebugOverlayState({
+        zoom: view.getZoom(),
+        center: transform(view.getCenter()!, view.getProjection(), 'EPSG:4326'),
+      });
+    };
+    map.on('moveend', updateViewState);
+    updateViewState();
+  }, [map]);
 
-  updateViewState = () => {
-    const view = this.props.map.getView();
-    this.setState({
-      zoom: view.getZoom(),
-      center: transform(view.getCenter()!, view.getProjection(), 'EPSG:4326'),
-    });
-  };
-
-  componentDidMount() {
-    this.props.map.on('moveend', this.updateViewState);
-    this.updateViewState();
-  }
-
-  render() {
-    const { zoom, center } = this.state;
-
-    return (
-      <div className={this.style.infoWrap}>
+  return (
+      <div className={style.infoWrap}>
         <table>
           <tbody>
             <tr>
               <th>Zoom:</th>
-              <td>{zoom?.toFixed(1)}</td>
+              <td>{debugOverlayState.zoom?.toFixed(1)}</td>
             </tr>
             <tr>
               <th>Center:&nbsp;</th>
               <td>
-                {center[0].toFixed(5)}, {center[1].toFixed(5)}
+                {debugOverlayState.center[0].toFixed(5)}, {debugOverlayState.center[1].toFixed(5)}
               </td>
             </tr>
           </tbody>
         </table>
       </div>
     );
-  }
 }
+
+// interface State {
+//   zoom?: number;
+//   center: Coordinate;
+// }
+
+// export class DebugOverlay extends PureComponent<Props, State> {
+//   style = useStyles2(getStyles);
+
+//   constructor(props: Props) {
+//     super(props);
+//     this.state = { zoom: 0, center: [0, 0] };
+//   }
+
+//   updateViewState = () => {
+//     const view = this.props.map.getView();
+//     this.setState({
+//       zoom: view.getZoom(),
+//       center: transform(view.getCenter()!, view.getProjection(), 'EPSG:4326'),
+//     });
+//   };
+
+//   componentDidMount() {
+//     this.props.map.on('moveend', this.updateViewState);
+//     this.updateViewState();
+//   }
+
+//   render() {
+//     const { zoom, center } = this.state;
+
+//     return (
+//       <div className={this.style.infoWrap}>
+//         <table>
+//           <tbody>
+//             <tr>
+//               <th>Zoom:</th>
+//               <td>{zoom?.toFixed(1)}</td>
+//             </tr>
+//             <tr>
+//               <th>Center:&nbsp;</th>
+//               <td>
+//                 {center[0].toFixed(5)}, {center[1].toFixed(5)}
+//               </td>
+//             </tr>
+//           </tbody>
+//         </table>
+//       </div>
+//     );
+//   }
+// }
 
 const getStyles = (theme: GrafanaTheme2) => ({
   infoWrap: css`
