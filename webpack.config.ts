105cd75fc094f7458550f37c5014e14426491c20
Felix Müller <felixrelleum@googlemail.com>
2026-02-13 00:17:30 +0100

    chore: fix import of webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,7 @@
 import type { Configuration } from 'webpack';
 import { merge } from 'webpack-merge';
-import grafanaConfig, { Env } from './.config/webpack/webpack.config.ts';
+import grafanaConfig from './.config/webpack/webpack.config.ts';
+import type { Env } from './.config/webpack/webpack.config.ts';
 
 const config = async (env: Env): Promise<Configuration> => {
   const baseConfig = await grafanaConfig(env);
