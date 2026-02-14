43b39e5e9eceaf794a53b656535ec80f161d6866
Felix Müller <felixrelleum@googlemail.com>
2026-02-14 22:14:40 +0100

    chore(minmaxzoomleveleditor): add second inline field row

--- src/editor/MinMaxZoomLevelEditor.tsx
+++ src/editor/MinMaxZoomLevelEditor.tsx
@@ -1,3 +1,4 @@
+import { StandardEditorProps } from "@grafana/data";
 import { Button, InlineField, InlineFieldRow, Input } from "@grafana/ui";
 import React, { useState } from "react";
 
@@ -6,34 +7,35 @@ export type MinMaxZoomLevelProps = {
     maxZoomLevel: number
 }
 
-export function MinMaxZoomLevelEditor({minZoomLevel, maxZoomLevel}: MinMaxZoomLevelProps, onChange: any) {
-    const [minZoom, setMinZoom] = useState(minZoomLevel ?? "");
-    const [maxZoom, setMaxZoom] = useState(maxZoomLevel ?? "");
+export function MinMaxZoomLevelEditor({value, onChange}: StandardEditorProps<MinMaxZoomLevelProps>) {
+    const [minZoom, setMinZoom] = useState(value.minZoomLevel ?? "");
+    const [maxZoom, setMaxZoom] = useState(value.maxZoomLevel ?? "");
     return (
         <>
             <InlineFieldRow>
-                <InlineField label="Min zoom level" invalid={minZoomLevel >= maxZoomLevel} 
-                error={minZoomLevel >= maxZoomLevel ? "min zoom level must be smaller than max zoom level": ""}>
+                <InlineField label="Min zoom level" invalid={minZoom >= maxZoom} 
+                error={minZoom >= maxZoom ? "min zoom level must be smaller than max zoom level": ""}>
                     <Input value={minZoom} type="number" placeholder="min zoom level" onChange={
                         (e) => {
                             setMinZoom(parseFloat(e.currentTarget.value));
                         }
                     } onBlur={() => {
-                        if (minZoomLevel >= maxZoomLevel) {
+                        if (minZoom >= maxZoom) {
                             return;
                         }
                     }}/>
                 </InlineField>
                 <Button>Set from current map view</Button>
-
-                <InlineField label="Max zoom level" invalid={minZoomLevel >= maxZoomLevel} 
-                error={minZoomLevel >= maxZoomLevel ? "min zoom level must be smaler than max zoom level": ""}>
+            </InlineFieldRow>
+            <InlineFieldRow>
+                <InlineField label="Max zoom level" invalid={minZoom >= maxZoom} 
+                error={minZoom >= maxZoom ? "min zoom level must be smaler than max zoom level": ""}>
                     <Input value={maxZoom} type="number" placeholder="max zoom level" onChange={
                         (e) => {
                             setMaxZoom(parseFloat(e.currentTarget.value));
                         }
                     } onBlur={() => {
-                        if (minZoomLevel >= maxZoomLevel) {
+                        if (minZoom >= maxZoom) {
                             return;
                         }
                     }}/>
